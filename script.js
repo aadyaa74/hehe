adc67aca4b7db4c42158e23a0906af576dc06cca
aadyaa74 <aadya.k2008@gmail.com>
2026-02-02 19:01:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const noButton = document.getElementById("no");
+
+noButton.addEventListener("mouseover", () => {
+  const x = Math.random() * 300 - 150;
+  const y = Math.random() * 300 - 150;
+
+  noButton.style.transform = `translate(${x}px, ${y}px)`;
+});
+
+const yesButton = document.getElementById("yes");
+
+yesButton.addEventListener("click", () => {
+  document.body.innerHTML = `
+    <div style="text-align:center;">
+      <h1>Yay!!! 💕🥰</h1>
+      <p>I knew you'd say yes 😌</p>
+    </div>
+  `;
+});
